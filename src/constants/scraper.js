d2ff2fbd46dbf75feeef7a0bb7f7b424a97829a7
stscoundrel <sampo@pixels.fi>
2020-08-21 13:57:16 +0300

    Constants: add dictionary urls

---
+++ src/constants/scraper.js
@@ -0,0 +1,7 @@
+const DICTIONARY_URL_EN2ON = 'https://www.vikingsofbjornstad.com/Old_Norse_Dictionary_E2N.shtm'
+const DICTIONARY_URL_ON2EN = 'https://www.vikingsofbjornstad.com/Old_Norse_Dictionary_N2E.shtm'
+
+module.exports = {
+  DICTIONARY_URL_EN2ON,
+  DICTIONARY_URL_ON2EN
+}
